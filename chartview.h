6ac9f3f0084c05207443542a34ba2c96ede9eadc
erekc <erek.r.cox@gmail.com>
2020-06-14 13:51:22 -0400

    simplified chart legends

--- chartview.h
+++ chartview.h
@@ -5,6 +5,13 @@
 #include <QObject>
 #include <QWidget>
 #include <QtCharts/QChartView>
+#include <QtCharts/QScatterSeries>
+#include <QtCharts/QLegendMarker>
+#include <QtGui/QImage>
+#include <QtGui/QPainter>
+#include <QtCore/QtMath>
+#include <vector>
+#include <memory>
 
 QT_CHARTS_USE_NAMESPACE
 
@@ -13,6 +20,8 @@ class ChartView : public QChartView
     Q_OBJECT
 public:
     explicit ChartView(QWidget *parent = 0);
+private:
+    std::vector<std::unique_ptr<QScatterSeries>> series;
 };
 
 #endif // CHARTVIEW_H
