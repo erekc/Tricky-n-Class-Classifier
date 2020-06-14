5e39d284ebbab44a454dbda78cbea79f2d7cf5e3
erekc <erek.r.cox@gmail.com>
2020-06-14 18:18:28 -0400

    change data points for chart view

--- main.cpp
+++ main.cpp
@@ -7,24 +7,25 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     xt::xarray<double> arr1 =
-    {{0, 6},
-     {2, 4},
-     {3, 8},
-     {7, 4},
-     {10, 5}};
+    {{0, 2},
+     {1, 2},
+     {2, 2},
+     {3, 2},
+     {4, 2}};
 
     xt::xarray<double> arr2 =
-    {{1, 1},
-     {3, 3},
-     {7, 6},
-     {8, 3},
-     {10, 2}};
+    {{0, 4},
+     {1, 4},
+     {2, 4},
+     {3, 4},
+     {4, 4}};
 
     xt::xarray<double> arr3 =
-    {{1, 5},
-     {4, 6},
-     {6, 3},
-     {9, 5}};
+    {{0, 6},
+     {1, 6},
+     {2, 6},
+     {3, 6},
+     {4, 6}};
 
     std::unique_ptr<ChartView> chartView = std::make_unique<ChartView>();
 
